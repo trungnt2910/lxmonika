bed7871f4b18026854e096574e631cab4c8f4de1
Trung <57174311+trungnt2910@users.noreply.github.com>
2024-04-03 09:02:05 +1100

    feat(reality): Implement Win32 reality device
    Connect the Windows interface for `\Device\Reality` to the shared
    functions in `reality.cpp`. `open`, `close`, `read`, `write`, and
    `seek` should currently be supported.
    
    Support for the `ioctl` operation is also added, but without any
    actual `ioctl` values implemented, the operation will always fail
    for now.

--- lxmonika/include/reality.h
+++ lxmonika/include/reality.h
@@ -9,3 +9,11 @@
 #endif
 
 #define RL_DEVICE_CODE (0x13EA1)
+
+#ifdef _WIN32
+// TODO: Unify the device type with RL_DEVICE_CODE instead of using FILE_DEVICE_UNKNOWN.
+#define RL_IOCTL_CODE(Function) \
+    CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800 | (Function), METHOD_BUFFERED, FILE_ANY_ACCESS)
+#else
+#define RL_IOCTL_CODE(Function) (Function)
+#endif
--- lxmonika/src/reality_win32.cpp
+++ lxmonika/src/reality_win32.cpp
@@ -1,9 +1,28 @@
 #include "reality.h"
 
+#include <ntstrsafe.h>
+#include <wdmsec.h>
+
+#include "AutoResource.h"
 #include "Logger.h"
+
+CONST UNICODE_STRING RlDeviceName = RTL_CONSTANT_STRING(L"\\Device\\Reality");
+static PDEVICE_OBJECT RlDeviceObject = NULL;
+
+//
+// Private function prototypes
+//
+
+static DRIVER_DISPATCH RlWin32DeviceCreate;
+static DRIVER_DISPATCH RlWin32DeviceClose;
+static DRIVER_DISPATCH RlWin32DeviceRead;
+static DRIVER_DISPATCH RlWin32DeviceWrite;
+static DRIVER_DISPATCH RlWin32DeviceControl;
+static DRIVER_DISPATCH RlWin32DeviceSetInformation;
 
 //
 // Lifetime functions
+// Not thread-safe, but this should be fine since they are only called by DriverEntry/DriverUnload.
 //
 
 extern "C"
@@ -14,7 +33,43 @@ RlpInitializeWin32Device(
 {
     UNREFERENCED_PARAMETER(DriverObject);
 
-    Logger::LogWarning("Win32 reality device is not implemented yet!");
+    if (RlDeviceObject != NULL)
+    {
+        return STATUS_SUCCESS;
+    }
+
+    DriverObject->MajorFunction[IRP_MJ_CREATE] = RlWin32DeviceCreate;
+    DriverObject->MajorFunction[IRP_MJ_CLOSE] = RlWin32DeviceClose;
+    DriverObject->MajorFunction[IRP_MJ_READ] = RlWin32DeviceRead;
+    DriverObject->MajorFunction[IRP_MJ_WRITE] = RlWin32DeviceWrite;
+    // ioctl
+    DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = RlWin32DeviceControl;
+    // seek
+    DriverObject->MajorFunction[IRP_MJ_SET_INFORMATION] = RlWin32DeviceSetInformation;
+
+    NTSTATUS status;
+
+    status = IoCreateDeviceSecure(
+        DriverObject,
+        0,
+        (PUNICODE_STRING)&RlDeviceName,
+        FILE_DEVICE_UNKNOWN,
+        FILE_DEVICE_SECURE_OPEN,
+        FALSE,
+        &SDDL_DEVOBJ_SYS_ALL_ADM_RWX_WORLD_RW_RES_R,
+        NULL,
+        &RlDeviceObject
+    );
+
+    if (!NT_SUCCESS(status))
+    {
+        if (RlDeviceObject != NULL)
+        {
+            IoDeleteDevice(RlDeviceObject);
+            RlDeviceObject = NULL;
+        }
+        return status;
+    }
 
     return STATUS_SUCCESS;
 }
@@ -23,5 +78,215 @@ extern "C"
 VOID
 RlpCleanupWin32Device()
 {
-    // Currently no-op.
+    if (RlDeviceObject != NULL)
+    {
+        IoDeleteDevice(RlDeviceObject);
+        RlDeviceObject = NULL;
+    }
+}
+
+//
+// Driver dispatch functions
+//
+
+static
+[[nodiscard]]
+NTSTATUS
+RlWin32CompleteRequest(
+    _Inout_ PIRP pIrp,
+    _In_ NTSTATUS status,
+    _In_ ULONG_PTR uInfo = 0
+)
+{
+    pIrp->IoStatus.Status = status;
+    pIrp->IoStatus.Information = uInfo;
+
+    IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+
+    return status;
+}
+
+static
+NTSTATUS
+RlWin32DeviceCreate(
+    _In_ PDEVICE_OBJECT pDeviceObject,
+    _Inout_ PIRP pIrp
+)
+{
+    Logger::LogTrace();
+
+    UNREFERENCED_PARAMETER(pDeviceObject);
+
+    NTSTATUS status = STATUS_SUCCESS;
+
+    PRL_FILE pNewFile = (PRL_FILE)ExAllocatePoolZero(PagedPool, sizeof(RL_FILE), MA_REALITY_TAG);
+    if (pNewFile == NULL)
+    {
+        return RlWin32CompleteRequest(pIrp, STATUS_NO_MEMORY);
+    }
+    AUTO_RESOURCE(pNewFile, [](auto p) { ExFreePoolWithTag(p, MA_REALITY_TAG); });
+
+    status = RlpFileOpen(pNewFile);
+
+    if (!NT_SUCCESS(status))
+    {
+        return RlWin32CompleteRequest(pIrp, status);
+    }
+
+    PIO_STACK_LOCATION pIrpStack = IoGetCurrentIrpStackLocation(pIrp);
+    pIrpStack->FileObject->FsContext = pNewFile;
+    pNewFile = NULL;
+
+    return RlWin32CompleteRequest(pIrp, STATUS_SUCCESS, FILE_OPENED);
+}
+
+static
+NTSTATUS
+RlWin32DeviceClose(
+    _In_ PDEVICE_OBJECT pDeviceObject,
+    _Inout_ PIRP pIrp
+)
+{
+    Logger::LogTrace();
+
+    UNREFERENCED_PARAMETER(pDeviceObject);
+
+    PIO_STACK_LOCATION pIrpStack = IoGetCurrentIrpStackLocation(pIrp);
+    ExFreePoolWithTag(pIrpStack->FileObject->FsContext, MA_REALITY_TAG);
+
+    return RlWin32CompleteRequest(pIrp, STATUS_SUCCESS);
+}
+
+static
+NTSTATUS
+RlWin32DeviceRead(
+    _In_ PDEVICE_OBJECT pDeviceObject,
+    _Inout_ PIRP pIrp
+)
+{
+    Logger::LogTrace();
+
+    UNREFERENCED_PARAMETER(pDeviceObject);
+
+    PIO_STACK_LOCATION pIrpStack = IoGetCurrentIrpStackLocation(pIrp);
+    PRL_FILE pFile = (PRL_FILE)pIrpStack->FileObject->FsContext;
+
+    INT64 iOffset = pIrpStack->Parameters.Read.ByteOffset.QuadPart;
+    SIZE_T szBytesTransfered = 0;
+
+    NTSTATUS status = RlpFileRead(
+        pFile,
+        pIrp->UserBuffer,
+        pIrpStack->Parameters.Read.Length,
+        &iOffset,
+        &szBytesTransfered);
+
+    return RlWin32CompleteRequest(pIrp, status, szBytesTransfered);
+}
+
+static
+NTSTATUS
+RlWin32DeviceWrite(
+    _In_ PDEVICE_OBJECT pDeviceObject,
+    _Inout_ PIRP pIrp
+)
+{
+    Logger::LogTrace();
+
+    UNREFERENCED_PARAMETER(pDeviceObject);
+
+    PIO_STACK_LOCATION pIrpStack = IoGetCurrentIrpStackLocation(pIrp);
+    PRL_FILE pFile = (PRL_FILE)pIrpStack->FileObject->FsContext;
+
+    INT64 iOffset;
+    if (pIrpStack->Parameters.Write.ByteOffset.LowPart == FILE_WRITE_TO_END_OF_FILE)
+    {
+        iOffset = pFile->Length;
+    }
+    else
+    {
+        iOffset = pIrpStack->Parameters.Write.ByteOffset.QuadPart;
+    }
+
+    SIZE_T szBytesTransfered = 0;
+
+    NTSTATUS status = RlpFileWrite(
+        pFile,
+        pIrp->UserBuffer,
+        pIrpStack->Parameters.Write.Length,
+        &iOffset,
+        &szBytesTransfered);
+
+    pIrp->IoStatus.Status = status;
+
+    return RlWin32CompleteRequest(pIrp, status, szBytesTransfered);
+}
+
+static
+NTSTATUS
+RlWin32DeviceControl(
+    _In_ PDEVICE_OBJECT pDeviceObject,
+    _Inout_ PIRP pIrp
+)
+{
+    Logger::LogTrace();
+
+    UNREFERENCED_PARAMETER(pDeviceObject);
+
+    PIO_STACK_LOCATION pIrpStack = IoGetCurrentIrpStackLocation(pIrp);
+    PRL_FILE pFile = (PRL_FILE)pIrpStack->FileObject->FsContext;
+
+    // For reality device ioctls, the method is always METHOD_BUFFERED
+    // (unlike read/write operations).
+    //
+    // Since we are providing a unified interface with the old LXSS reality device,
+    // we want to be able to allow both the driver and the user code to share
+    // a single buffer for input and output.
+
+    NTSTATUS status = RlpFileIoctl(
+        pFile,
+        METHOD_FROM_CTL_CODE(0x800 ^ pIrpStack->Parameters.DeviceIoControl.IoControlCode),
+        pIrp->AssociatedIrp.SystemBuffer
+    );
+
+    return RlWin32CompleteRequest(pIrp, status);
+}
+
+static
+NTSTATUS
+RlWin32DeviceSetInformation(
+    _In_ PDEVICE_OBJECT pDeviceObject,
+    _Inout_ PIRP pIrp
+)
+{
+    Logger::LogTrace();
+
+    UNREFERENCED_PARAMETER(pDeviceObject);
+
+    PIO_STACK_LOCATION pIrpStack = IoGetCurrentIrpStackLocation(pIrp);
+    PRL_FILE pFile = (PRL_FILE)pIrpStack->FileObject->FsContext;
+
+    NTSTATUS status = STATUS_SUCCESS;
+
+    switch (pIrpStack->Parameters.SetFile.FileInformationClass)
+    {
+    case FilePositionInformation:
+    {
+        PFILE_POSITION_INFORMATION pFilePositionInfo =
+            (PFILE_POSITION_INFORMATION)pIrp->AssociatedIrp.SystemBuffer;
+        status = RlpFileSeek(pFile, pFilePositionInfo->CurrentByteOffset.QuadPart, SEEK_SET, NULL);
+        if (!NT_SUCCESS(status))
+        {
+            return RlWin32CompleteRequest(pIrp, status);
+        }
+
+        // "The Information member receives the number of bytes set on the file."
+        return RlWin32CompleteRequest(pIrp, STATUS_SUCCESS, sizeof(FILE_POSITION_INFORMATION));
+    }
+    break;
+    default:
+    {
+        return RlWin32CompleteRequest(pIrp, STATUS_NOT_SUPPORTED);
+    }
+    }
 }
