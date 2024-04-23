f225e4d233e207ee3fc6ce36de76c678542603da
Trung <57174311+trungnt2910@users.noreply.github.com>
2024-04-23 10:34:54 +1000

    chore(host): Remove dot from display name
    Display names are not descriptions. Most of these names for Windows
    drivers do not contain the dot.

--- monika/include/constants.h
+++ monika/include/constants.h
@@ -5,5 +5,5 @@
 // Constants shared among the host components
 
 #define MA_SERVICE_NAME					L"lxmonika"
-#define MA_SERVICE_DISPLAY_NAME			L"Just Monika."
+#define MA_SERVICE_DISPLAY_NAME			L"Just Monika"
 #define MA_SERVICE_DEFAULT_DRIVER_NAME	L"lxmonika.sys"
