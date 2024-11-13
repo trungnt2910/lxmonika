5bae2030b01a5967b10dab229cbbf2ecdb99e2ea
Trung <57174311+trungnt2910@users.noreply.github.com>
2024-11-14 00:58:03 +1100

    feat(host): Check for `lxcore.sys`
    The `monika.exe` CLI now knows how to check for `lxcore.sys`. It also
    allows the user to install a copy of `lxcore.sys` in the `install`
    command.
    
    `lxcore.sys` can be ignored with the `--force` switch.

--- monika/include/constants.h
+++ monika/include/constants.h
@@ -7,3 +7,4 @@
 #define MA_SERVICE_NAME             L"lxmonika"
 #define MA_SERVICE_DISPLAY_NAME     L"Just Monika"
 #define MA_SERVICE_DRIVER_NAME      L"lxmonika.sys"
+#define MA_CORE_DRIVER_NAME         L"lxcore.sys"
