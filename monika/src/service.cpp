939811f947732b63439c1ac3150acc8d21afb4d1
Trung <57174311+trungnt2910@users.noreply.github.com>
2024-11-14 00:48:40 +1100

    chore(host): Extract `Transaction` class
    Extract this utility class to its own `Transaction.h` header, allowing
    more files to make use of the class.

--- monika/src/service.cpp
+++ monika/src/service.cpp
@@ -1,7 +1,6 @@
 #include "service.h"
 
 #include <filesystem>
-#include <functional>
 
 #include <Windows.h>
 #include <winsvc.h>
@@ -12,38 +11,9 @@
 #include "util.h"
 
 #include "Exception.h"
+#include "Transaction.h"
 
 #define MA_SERVICE_OLD_KEY_PREFIX L"MaOld"
-
-class Transaction
-{
-private:
-    bool _commit = false;
-    std::function<void()> _cleanup;
-public:
-    Transaction(
-        const std::function<void()>& action,
-        std::function<void()>&& cleanup
-    ) : _cleanup(std::move(cleanup))
-    {
-        action();
-    }
-
-    Transaction(const Transaction&) = delete;
-
-    void Commit()
-    {
-        _commit = true;
-    }
-
-    ~Transaction()
-    {
-        if (!_commit)
-        {
-            _cleanup();
-        }
-    }
-};
 
 static
 std::wstring
