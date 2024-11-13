79c5dd65d3c1aeb9ac6ede95e4667433e547d00f
Trung <57174311+trungnt2910@users.noreply.github.com>
2024-11-14 01:42:25 +1100

    feat(host): Clean up if `lxcore.sys` is ours
    Add an alternate data stream to tag `lxcore.sys` if it is copied by
    `monika install`.
    
    If the tag is present, `monika uninstall` will delete this file.

--- monika/include/constants.h
+++ monika/include/constants.h
@@ -8,3 +8,4 @@
 #define MA_SERVICE_DISPLAY_NAME     L"Just Monika"
 #define MA_SERVICE_DRIVER_NAME      L"lxmonika.sys"
 #define MA_CORE_DRIVER_NAME         L"lxcore.sys"
+#define MA_CORE_COPIED_STREAM_NAME	L"MaCoreCopied"
